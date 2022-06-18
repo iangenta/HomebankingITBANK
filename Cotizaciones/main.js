68da39b92e8e3aa7b5b6f267072490c7120b30d9
iangenta <gentaian2@gmail.com>
2022-06-18 15:38:50 -0300

    Se creó un archivo js que recupera una API para imprimir los valores de las cotizaciones

---
+++ Cotizaciones/main.js
@@ -0,0 +1,28 @@
+
+var contenido = document.querySelector('#contenido')
+
+fetch('https://www.dolarsi.com/api/api.php?    type=valoresprincipales')
+.then(res => res.json())
+.then(datos => {
+    // console.log(datos)
+    tabla(datos)
+})
+
+
+function tabla(datos) {
+    // console.log(datos)
+    contenido.innerHTML = ''
+    for(let valor of datos){
+        // console.log(valor.nombre)
+        contenido.innerHTML += `
+        
+        <tr>
+            <th scope="row">${ valor.casa.nombre }</th>
+            <td>${ valor.casa.compra }</td>
+            <td>${ valor.casa.venta }</td>
+            <td>${ valor.casa.variacion}</td>
+        </tr>
+        
+        `
+    }
+}
